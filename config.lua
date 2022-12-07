f42d2f343898146c98eb5fea22ed1db8e3da6c7c
RDOG Designs <120055281+RDOGDesigns@users.noreply.github.com>
2022-12-07 14:20:50 -0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = Config or {}
+
+Config.SnowBallsAmount = 2 -this is the amount you get per pickup
+Config.Key = 119 -key to press to pickup snow balls
+Config.UseStaticXMASWeather = false -trigers snow when restarting script
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'Commands By RDOG Designs'
+
+version '1.0'
+
+client_scripts{
+     'config.lua',
+     'client.lua'
+}
